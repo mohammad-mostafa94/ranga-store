3cd44eb7a674eaff4c7d055cb7e52d09e0c30653
Mohammad Mostafa <mohammad-mostafa94>
2021-09-27 06:26:10 +0600

    image part fixed

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
